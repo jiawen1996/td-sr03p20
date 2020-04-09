939133abef4f20a855c6a0126887e322a1cbb54c
lyujiawe <jiawen.lyu@etu.utc.fr>
2020-04-09 17:03:32 +0200

    create package thread in server-chat

--- devoir1/serveur-chat/src/Server.java
+++ devoir1/serveur-chat/src/Server.java
@@ -1,11 +1,9 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
-import java.util.Vector;
+
+import thread.MessageReceptor;
 
 public class Server {
 	
--- devoir1/serveur-chat/src/thread/MessageReceptor.java
+++ devoir1/serveur-chat/src/thread/MessageReceptor.java
@@ -1,5 +1,4 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+package thread;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
